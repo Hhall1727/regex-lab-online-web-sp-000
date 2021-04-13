09f78b73abf1d1f8688f42b6e3b1702c4f6e0868
Halie Hall <halierae17@outlook.com>
2021-04-13 19:51:05 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,35 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\A+[aeiouAEIOU]/)
+    true
+  else 
+     false 
+  end  
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  def count
+  end
+ if text.scan(/\A[un]+\z[ing]/)
+  return true
+ end
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/\A[A-Z]+\z[\W]/)
+    return true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+if phone.scan(/(\d+)-(\d+)-(\d+)/)
+  true
+else
+  false
+end
 end
