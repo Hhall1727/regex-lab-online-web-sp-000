3ac8e6eff2190c132703591e021c813fd2c0fa18
Halie Hall <halierae17@outlook.com>
2021-04-13 19:59:52 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-if phone.match(/^[(]?\d{3}[)]?[\s]?/)
+if phone.match(/^[(]?\d{3}[)]?[\s]?\d{3}[\s-]/)
   true
 else
   false
